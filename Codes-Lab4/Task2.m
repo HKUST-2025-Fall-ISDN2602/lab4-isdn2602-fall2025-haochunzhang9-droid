444218ebea4951f0dd8f7eba9673d6dad6e8fd75
ZHANG Haochun <haochunzhang9@gmail.com>
2025-10-28 15:53:51 +0800

    change2

--- Codes-Lab4/Task2.m
+++ Codes-Lab4/Task2.m
@@ -30,8 +30,7 @@ title('Histogram of the runs from 0 to 9 and >= 10');
 
 
      % % % % Revise the following code to generate a valid  and efficient dictionary   % % % %  
-     dict = {[0 0 0 0], [0 0 0 1], [0 0 1 0], [0 0 1 1], [0 1 0 0],...
-         [0 1 0 1], [0 1 1 0], [0 1 1 1], [1 0 0 0], [1 0 0 1],[1 0 1 0]};
+        dict = huffman_encode(runs,1);
        
     % % % % Do not change the code below % % % %  
 
